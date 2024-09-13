c58f528c9ad15f0518bf329b1311e998014de3c5
LAPTOP-FE02CMJA\Administrator <2579807488@qq.com>
2024-09-13 11:20:03 +0800

    Revert "fix: 取消开发环境限制"
    This reverts commit 07f8a4118ce3fb599d5a93cfaa77efe83d425bd7.

--- src/common/guards/demo-environment.guard.ts
+++ src/common/guards/demo-environment.guard.ts
@@ -1,4 +1,15 @@
-// 判断是否为开发环境，并进行限制
+/*
+
+ * @Date: 2022-01-29 11:12:00
+ * @LastEditTime: 2022-09-18 11:07:12
+ * @LastEditors: Please set LastEditors
+ * @Description: 是否演示环境守卫
+ * @FilePath: /meimei-admin/src/common/guards/demo-environment.guard.ts
+ * You can you up，no can no bb！！
+ */
+/*
+https://docs.nestjs.com/guards#guards
+*/
 
 import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
@@ -12,32 +23,30 @@ export class DemoEnvironmentGuard implements CanActivate {
     private readonly jwtService: JwtService,
   ) {}
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    // 判断nest.js环境是否为开发环境
     const isDemoEnvironment =
       this.configService.get<boolean>('isDemoEnvironment');
     if (!isDemoEnvironment) return true;
-    // 获取请求头
     const request: Request = context.switchToHttp().getRequest();
+    // 此处因演示环境原因 处理特殊用户 不限制接口, 此块代码可以根据实际情况移除
     const authorization = request.headers['authorization'];
     if (!authorization) return true;
-    // userId为1则不限制
     const payload = await this.jwtService.verify(authorization.slice(7));
     if (+payload?.userId === -1) return true;
     // === end
-    // const allowUrlArr = [
-    //   '/login',
-    //   '/logout',
-    //   '/web/user/login',
-    //   '/web/user/reg',
-    //   '/user-collect',
-    //   '/user-rate',
-    //   '/user-movie',
-    // ]; //放过的路由
-    // if (
-    //   request.method.toLocaleLowerCase() != 'get' &&
-    //   !allowUrlArr.includes(request.url)
-    // )
-    //   throw new ApiException('演示环境,不允许操作');
+    const allowUrlArr = [
+      '/login',
+      '/logout',
+      '/web/user/login',
+      '/web/user/reg',
+      '/user-collect',
+      '/user-rate',
+      '/user-movie',
+    ]; //放过的路由
+    if (
+      request.method.toLocaleLowerCase() != 'get' &&
+      !allowUrlArr.includes(request.url)
+    )
+      throw new ApiException('演示环境,不允许操作');
     return true;
   }
 }
